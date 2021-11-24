6549f8da02c7069c1fdf516112986aa82a5c2424
tatarints <tvlt2512@gmail.com>
2021-11-25 00:28:28 +0500

    Добавьте файлы проекта.

---
+++ lesson_3/Program.cs
@@ -0,0 +1,16 @@
+﻿string str = @"Сладких снов и сновидений, Татьяна";
+char[] ar = str.ToCharArray();
+
+int c = ar.Length;               // длина массива
+int k = c / 2;                   // середина массива
+char lastIndex;                  // вспомогательный элемент для обмена значениями
+for (int i = 0; i < k; i++)
+{
+    lastIndex = ar[i];
+    ar[i] = ar[c - i - 1];
+    ar[c - i - 1] = lastIndex;
+}
+foreach (int i in ar)
+{
+    Console.Write((char)(i));
+}
